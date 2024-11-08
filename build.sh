ceb99c1059e782d086b53b35b3b9d3fd689e6ac6
ripples <564311873@qq.com>
2024-11-08 08:09:55 +0000

    feat: optimize build script

--- build.sh
+++ build.sh
@@ -1,5 +1,17 @@
 #!/bin/sh
 
+BUILDER=caddy-builder
+
 docker pull caddy:2-builder-alpine
-docker build . -t ripples/caddy
-docker push ripples/caddy
+
+docker buildx create --use --name $BUILDER
+docker buildx inspect --bootstrap
+
+docker buildx build \
+    --platform linux/amd64,linux/arm64 \
+    --output "type=image,push=true" \
+    --tag ripples/caddy \
+    --builder $BUILDER .
+
+docker buildx stop $BUILDER
+docker buildx rm $BUILDER
